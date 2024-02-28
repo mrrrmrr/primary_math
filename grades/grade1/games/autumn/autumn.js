8e0c31842156f79a05f41fa8c465225234d660d4
alina <ershovaalina25@gmail.com>
2024-02-28 13:24:32 +0300

    right leaf modified

--- grades/grade1/games/autumn/autumn.js
+++ grades/grade1/games/autumn/autumn.js
@@ -1,5 +1,9 @@
 let answer, wrongNum = 0, rightNum = 0;
 
+function pxToNumber(px){
+    return Number(px.slice(0, px.length - 2));
+}
+
 
 function setTask(){
     let sum = getRandomInteger(2, 20);
@@ -30,13 +34,49 @@ function setTask(){
 }
 
 
+function separateLeaf(containerElement){
+    let containerStyle = window.getComputedStyle(containerElement)
+    let x = containerStyle.left;
+    let y = containerStyle.top;
+
+    rightLeafElement = containerElement.cloneNode(true);
+    containerElement.lastElementChild.remove();
+    containerElement.lastElementChild.remove();
+    containerElement.classList.remove('clickable');
+    containerElement.onclick = null;
+
+    rightLeafElement.classList.remove('flight');
+    rightLeafElement.firstElementChild.remove();
+    rightLeafElement.firstElementChild.setAttribute('src', "images/leaf_yellow.svg")
+    rightLeafElement.style.top = y;
+    rightLeafElement.style.left = x;
+    document.body.append(rightLeafElement);
+    return rightLeafElement;
+}
+
+
+function setLeafFall(leafElement){
+    let y = pxToNumber(window.getComputedStyle(leafElement).top);
+    let y_max = pxToNumber(window.getComputedStyle(document.body).height);
+
+    let step = 1;
+    let time = 1;
+
+    let fall = setInterval( function () {
+        y += step;
+        leafElement.style.top = `${y}px`;
+        if (y >= y_max) clearInterval(fall);
+    }, time);
+}
+
+
 setInterval(function () {
     let n = getRandomInteger(1, 20)
 
     let birdElement = document.createElement('div');
     let y = getRandomInteger(0, 80);
     birdElement.style.top = `${y}%`;
-    birdElement.className = 'birdContainer';
+    birdElement.className = 'birdContainer flight clickable';
     birdElement.setAttribute('data', n);
     birdElement.innerHTML = `
         <img src="images/bird.gif" class="bird">
@@ -49,6 +89,9 @@ setInterval(function () {
             rightNum++;
             document.getElementById('right').innerHTML = rightNum;
             setTask();
+
+            let leafElement = separateLeaf(this);
+            setLeafFall(leafElement);
         }
         else{
             wrongNum++;
